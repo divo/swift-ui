f36ac7bc565905740621884761d61697bb743204
divo <divoapple@gmail.com>
2023-06-09 11:26:49 +0100

    Starting from scratch on PhotoPicker

--- ImageUploadTest/ImageUploadTest/ContentView.swift
+++ ImageUploadTest/ImageUploadTest/ContentView.swift
@@ -7,76 +7,22 @@
 
 import SwiftUI
 import PhotosUI
-import UIKit
 
-struct ContentView: View {
-    @State private var isShowingImagePicker = false
-    @State private var selectedImage: UIImage?
-
-    var body: some View {
-        VStack {
-            Button(action: {
-                isShowingImagePicker = true
-            }) {
-                Text("Select Image")
-            }
-            .sheet(isPresented: $isShowingImagePicker) {
-                ImagePicker(selectedImage: $selectedImage)
-            }
-
-            if let image = selectedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-            }
-            
-            Button(action: {
-                if let image = selectedImage {
-                    uploadImage(image)
-                }
-            }) {
-                Text("Upload Image")
-            }
-        }
+class PhotoModel: ObservableObject {
+  @Published var imageSelection: PhotosPickerItem? = nil {
+    didSet {
+      if let imageSelection {
+      }
     }
-
-    func uploadImage(_ image: UIImage) {
-        // Perform the image upload
-        // ...
-    }
+  }
 }
 
-struct ImagePicker: UIViewControllerRepresentable {
-    @Environment(\.presentationMode) private var presentationMode
-    @Binding var selectedImage: UIImage?
-
-    func makeUIViewController(context: Context) -> PHPickerViewController {
-        var configuration = PHPickerConfiguration()
-        configuration.selectionLimit = 1
-        let picker = PHPickerViewController(configuration: configuration)
-        picker.delegate = context.coordinator
-        return picker
+struct ContentView: View {
+  @ObservedObject var viewModel = PhotoModel()
+  
+  var body: some View {
+    PhotosPicker(selection: $viewModel.imageSelection, matching: .images, photoLibrary: .shared()) {
+      Text("Select Photos")
     }
-
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, PHPickerViewControllerDelegate {
-        let parent: ImagePicker
-
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-
-        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            parent.presentationMode.wrappedValue.dismiss()
-
-            guard let itemProvider = results.first?.itemProvider else { return }
-
-        }
-    }
+  }
 }
