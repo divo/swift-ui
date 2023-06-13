4dcf958b0831f4c76f9d5c0318844f8c43af65c7
divo <divoapple@gmail.com>
2023-06-13 21:49:44 +0100

    POC Grabbing cookies

--- WebViewExperiment/WebViewExperiment/ContentView.swift
+++ WebViewExperiment/WebViewExperiment/ContentView.swift
@@ -10,8 +10,20 @@ import WebKit
 import UIKit
 
 struct ContentView: View {
+//  let webView = WebView(url: URL(string: "http://192.168.0.87:3000/photo_albums")!)
+  let webView = WebView(url: URL(string: "http://localhost:3000/photo_albums")!)
+  
     var body: some View {
-      WebView(url: URL(string: "https://mementos.ink")!)
+      Button("Debug") {
+        // TODO: Intercept all calls to the auth controllers and grab the cookies after?
+        // Or if there are no cookies, force a trip to authenticate and then grab the cookie
+        // manage the authenticaion lifecycle on client
+        webView.webDataStore.httpCookieStore.getAllCookies { cookies in
+          print("Cookies")
+        }
+        print("Debug")
+      }
+      webView
     }
 }
 
@@ -23,9 +35,20 @@ struct ContentView_Previews: PreviewProvider {
 
 struct WebView: UIViewRepresentable {
   let url: URL
+  let webDataStore = WKWebsiteDataStore.default()
+  let configuration: WKWebViewConfiguration
+  let webView: WKWebView
+  
+  init(url: URL) {
+    self.url = url
+    self.configuration = WKWebViewConfiguration()
+    configuration.websiteDataStore = webDataStore
+    self.webView = WKWebView(frame: .zero, configuration: configuration)
+  }
   
   func makeUIView(context: Context) -> some UIView {
-    return WKWebView()
+    webView.customUserAgent = "Memenots-iOS"
+    return webView
   }
   
   func updateUIView(_ uiView: UIViewType, context: Context) {
